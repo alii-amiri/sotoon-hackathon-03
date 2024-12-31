9f3453cab3265f0883537b5c7523d3f86a06168c
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 03:11:35 +0330

    feat: Register models in core/admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from core.models import Category, EmailTemplate
+from users.models import CustomUser, Group
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'image', 'created_at', 'updated_at')
+    search_fields = ('title',)
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    filter_horizontal = ('members',)  # For a better UI to select members
+
+@admin.register(EmailTemplate)
+class EmailTemplateAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'created_at')
+    search_fields = ('title',)
+    list_filter = ('category',)
