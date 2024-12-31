f0f73a32b2784cd97d7574de2e8309d9626d16d7
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 03:13:34 +0330

    feat: Add core models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Category (models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='categories/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class EmailTemplate(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    content = models.TextField()  # Stores HTML
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='templates')
+
+    def __str__(self):
+        return self.title
+    
+    def render(self, context):
+        """
+        Render the template content with the given context.
+        :param context: A dictionary of key-value pairs for placeholders.
+        """
+        from django.template import Template, Context
+        template = Template(self.content)
+        return template.render(Context(context))
+
+    class Meta:
+        app_label = 'core'
