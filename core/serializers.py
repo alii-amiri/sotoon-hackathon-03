f3b1eed76d755a8eb96e4041b744d3417711a549
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 12:19:37 +0330

    feat: Add EmailTemplateSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, EmailTemplate
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
+        fields = '__all__'
+
+class EmailTemplateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EmailTemplate
         fields = '__all__'
