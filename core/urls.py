49c7301894b3da03ec94d7df2b49296323354783
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 14:38:51 +0330

    feat: Add a DefaultRouter to handle the urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from .views import DynamicVariablesView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DynamicVariablesView, CategoryViewSet, EmailTemplateViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'templates', EmailTemplateViewSet, basename='template')
 
 urlpatterns = [
     path('dynamic-variables/', DynamicVariablesView.as_view(), name='dynamic-variables'),
+    path('', include(router.urls)),
 ]
