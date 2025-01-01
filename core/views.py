efe80d42a90ea321558207800ca64a1c9d610fbd
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 11:42:12 +0330

    feat: Add DynamicVariablesView to path

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from core.constants import DynamicVariables
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class DynamicVariablesView(APIView):
+    """
+    Exposes the list of dynamic variables as an API endpoint
+    """
+    def get(self, request):
+        variables = [var.value for var in DynamicVariables]
+        return Response({"variables": variables})
