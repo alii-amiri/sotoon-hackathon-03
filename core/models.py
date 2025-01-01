0a7e8550e4a7cdbf7e776c255e4e53c8f29a43a5
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 11:35:15 +0330

    fix: Add timestamp fields to EmailTemplate

--- core/models.py
+++ core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.template import Template, Context
+from core.constants import DynamicVariables
 
 class Category (models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -10,19 +12,20 @@ class Category (models.Model):
 class EmailTemplate(models.Model):
     title = models.CharField(max_length=255, unique=True)
     content = models.TextField()  # Stores HTML
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name='templates')
-
-    def __str__(self):
-        return self.title
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, related_name='templates')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     def render(self, context):
         """
         Render the template content with the given context.
         :param context: A dictionary of key-value pairs for placeholders.
         """
-        from django.template import Template, Context
+        allowed_variables = [var.value for var in DynamicVariables]
+        filtered_context = {key: value for key, value in context.items() if key in allowed_variables}
         template = Template(self.content)
-        return template.render(Context(context))
+        return template.render(Context(filtered_context))
 
     class Meta:
         app_label = 'core'
