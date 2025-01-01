9f27dce2d0db606780eabfbbcdcfbfa0cbd01dae
Ali Amiri <aliamirii.am@gmail.com>
2025-01-01 12:26:18 +0330

    feat: Add ViewSets for EmailTemplate and Category

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
+from rest_framework import viewsets
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from core.constants import DynamicVariables
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, EmailTemplate
+from .serializers import CategorySerializer, EmailTemplateSerializer
 
 class DynamicVariablesView(APIView):
     """
@@ -13,3 +14,12 @@ class DynamicVariablesView(APIView):
     def get(self, request):
         variables = [var.value for var in DynamicVariables]
         return Response({"variables": variables})
+
+#   ViewSets for CRUD operations 
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class EmailTemplateViewSet(viewsets.ModelViewSet):
+    queryset = EmailTemplate.objects.all()
+    serializer_class = EmailTemplateSerializer
